55fa955e3f77d970872d6b7bf183b274a50c4ce6
William J Laffin <william.laffin@gmail.com>
2015-12-21 23:26:58 -0500

    Make it a png, add anti-alias, add dogo, tweak UI

--- harmonograph.go
+++ harmonograph.go
@@ -1,11 +1,12 @@
 // Harmonograph generates GIF animations of random Harmonograph immitation figures.
-// this file started as https://github.com/adonovan/gopl.io/blob/master/ch1/lissajous/main.go
+// run https://github.com/liudng/dogo to have a watcher restart the app on change.
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
-	"image/gif"
+	"image/png"
 	"io"
 	"io/ioutil"
 	"log"
@@ -23,7 +24,7 @@ const (
 
 func main() {
 	http.HandleFunc("/harmonograph.html", servePage("harmonograph.html"))
-	http.HandleFunc("/gif", serveHarmonograph)
+	http.HandleFunc("/png", serveHarmonograph)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
@@ -33,6 +34,7 @@ func servePage(filename string) func(w http.ResponseWriter, r *http.Request) {
 		log.Fatalf("servePage: %v\n", err)
 	}
 	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
 		_, _ = w.Write(data)
 	}
 }
@@ -51,35 +53,71 @@ func serveHarmonograph(w http.ResponseWriter, r *http.Request) {
 	Y1 := dampedPendulum(getFloat(r, "Y1f"), getFloat(r, "Y1p"), getFloat(r, "Y1A"), getFloat(r, "Y1d"))
 	Y2 := dampedPendulum(getFloat(r, "Y2f"), getFloat(r, "Y2p"), getFloat(r, "Y2A"), getFloat(r, "Y2d"))
 	tmax := getFloat(r, "tmax")
-
+	w.Header().Set("Content-Type", "image/png")
 	harmonograph(X1, X2, Y1, Y2, tmax, w)
 }
 
 func harmonograph(X1, X2, Y1, Y2 func(float64) float64, tmax float64, out io.Writer) {
 	const (
-		cycles  = 4     // number of complete x oscillator revolutions
-		res     = 0.001 // angular resolution
-		size    = 100   // image canvas covers [-size..+size]
-		nframes = 64    // number of animation frames
-		delay   = 10    // delay between frames in 10ms units
+		res       = 0.001 // angular resolution
+		size      = 100   // image canvas covers [-size..+size]
+		alphaStep = 50
 	)
-	// freq := rand.Float64() * 3.0 // relative frequence of y oscillator
-	anim := gif.GIF{LoopCount: nframes}
-	// phase := 0.0 // phase difference
-	for i := 0; i < nframes; i++ {
-		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
-		img := image.NewPaletted(rect, palette)
-
-		for t := 0.0; t < tmax; t += res {
-			x := X1(t) + X2(t)
-			y := Y1(t) + Y2(t)
-			img.SetColorIndex(int(x), int(y), blackIndex)
+	rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+	img := image.NewRGBA(rect)
+	for alx := 0; alx < 2*size+1; alx++ {
+		for aly := 0; aly < 2*size+1; aly++ {
+			img.SetRGBA(alx, aly, color.RGBA{0, 0, 0, 0})
 		}
-		// phase += 0.1
-		anim.Delay = append(anim.Delay, delay)
-		anim.Image = append(anim.Image, img)
 	}
-	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+	printx := 0
+	printy := 0
+	maxPrint := 10
+	xprev := math.Inf(-1)
+	yprev := math.Inf(-1)
+
+	for t := 0.0; t < tmax; t += res {
+		x := size + X1(t) + X2(t)
+		if printx < maxPrint {
+			fmt.Printf("X: %#v\n", x)
+			printx++
+		}
+		if x > 2*size+1 {
+			xprev = math.Inf(-1)
+			yprev = math.Inf(-1)
+			continue
+		}
+		if x < 0 {
+			xprev = math.Inf(-1)
+			yprev = math.Inf(-1)
+			continue
+		}
+		y := size + Y1(t) + Y2(t)
+		if printy < maxPrint {
+			fmt.Printf("Y: %#v\n", y)
+			printy++
+		}
+		if y > 2*size+1 {
+			xprev = math.Inf(-1)
+			yprev = math.Inf(-1)
+			continue
+		}
+		if y < 0 {
+			xprev = math.Inf(-1)
+			yprev = math.Inf(-1)
+			continue
+		}
+		if xprev != math.Inf(-1) {
+			xiolin_wu_draw_line(xprev, yprev, x, y, img)
+		}
+		xprev = x
+		yprev = y
+	}
+	png.Encode(out, img) // NOTE: ignoring encoding errors
+}
+
+func importance(x1, y1, x2, y2 float64) float64 {
+	return 1 - math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
 }
 
 func dampedPendulum(frequency, phase, amplitude, damping float64) func(time float64) float64 {
@@ -91,4 +129,96 @@ func dampedPendulum(frequency, phase, amplitude, damping float64) func(time floa
 		t := time
 		return A * math.Sin(t*f+p) * math.Exp(-1*d*t)
 	}
+}
+
+//https://en.wikipedia.org/wiki/Xiaolin_Wu%27s_line_algorithm
+func xiolin_wu_draw_line(x0, y0, x1, y1 float64, img *image.RGBA) {
+	steep := math.Abs(y1-y0) > math.Abs(x1-x0)
+	if steep {
+		// swap
+		x0, y0 = y0, x0
+		x1, y1 = y1, x1
+	}
+	if x0 > x1 {
+		x0, x1 = x1, x0
+		y0, y1 = y1, y0
+	}
+	dx := x1 - x0
+	dy := y1 - y0
+	gradient := dy / dx
+
+	// handle first endpoint
+	xend := xiolin_round(x0)
+	yend := y0 + gradient*(xend-x0)
+	xgap := xiolin_rfpart(x0 + 0.5)
+	xpxl1 := xend // this will be used in the main loop
+	ypxl1 := xiolin_ipart(yend)
+	if steep {
+		xiolin_plot(ypxl1, xpxl1, xiolin_rfpart(yend)*xgap, img)
+		xiolin_plot(ypxl1+1, xpxl1, xiolin_fpart(yend)*xgap, img)
+	} else {
+		xiolin_plot(xpxl1, ypxl1, xiolin_rfpart(yend)*xgap, img)
+		xiolin_plot(xpxl1, ypxl1+1, xiolin_fpart(yend)*xgap, img)
+	}
+	intery := yend + gradient // first y-intersection for the main loop
+
+	// handle second endpoint
+	xend = xiolin_round(x1)
+	yend = y1 + gradient*(xend-x1)
+	xgap = xiolin_fpart(x1 + 0.5)
+	xpxl2 := xend //this will be used in the main loop
+	ypxl2 := xiolin_ipart(yend)
+	if steep {
+		xiolin_plot(ypxl2, xpxl2, xiolin_rfpart(yend)*xgap, img)
+		xiolin_plot(ypxl2+1, xpxl2, xiolin_fpart(yend)*xgap, img)
+	} else {
+		xiolin_plot(xpxl2, ypxl2, xiolin_rfpart(yend)*xgap, img)
+		xiolin_plot(xpxl2, ypxl2+1, xiolin_fpart(yend)*xgap, img)
+	}
+
+	// main loop
+	for x := xpxl1 + 1.0; x <= xpxl2-1; x++ {
+		if steep {
+			xiolin_plot(xiolin_ipart(intery), x, xiolin_rfpart(intery), img)
+			xiolin_plot(xiolin_ipart(intery)+1, x, xiolin_fpart(intery), img)
+		} else {
+			xiolin_plot(x, xiolin_ipart(intery), xiolin_rfpart(intery), img)
+			xiolin_plot(x, xiolin_ipart(intery)+1, xiolin_fpart(intery), img)
+		}
+		intery = intery + gradient
+	}
+}
+
+func xiolin_plot(x, y, c float64, img *image.RGBA) {
+	al := saturating_add(float64(img.RGBAAt(int(x), int(y)).A), 255.0*c)
+	img.SetRGBA(int(x), int(y), color.RGBA{al, 0, 0, al})
+}
+
+func saturating_add(x, y float64) uint8 {
+	if x+y > 255 {
+		return 255
+	}
+	return uint8(x + y)
+}
+
+func xiolin_ipart(x float64) float64 {
+	if x >= 0.0 {
+		return math.Floor(x)
+	}
+	return math.Ceil(x)
+}
+
+func xiolin_round(x float64) float64 {
+	return xiolin_ipart(x + 0.5)
+}
+
+func xiolin_fpart(x float64) float64 {
+	if x < 0.0 {
+		return 1.0 - (x - math.Floor(x))
+	}
+	return x - math.Floor(x)
+}
+
+func xiolin_rfpart(x float64) float64 {
+	return 1 - xiolin_fpart(x)
 }
